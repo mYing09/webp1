c23fa9d86561cb43a532299195591db856e09caf
chelizi123 <che_li_zi@163.com>
2019-11-15 11:10:34 +0800

    打包上传

---
+++ app/app.js
@@ -0,0 +1,17 @@
+// 加载app.less
+require('../less/app.less');
+
+window.onload = function(){
+
+    let person = ['kevin','mark','iven'];
+
+    let ul = document.createElement('ul');
+
+    person.map(v => {
+        let li = this.document.createElement('li');
+        li.textContent = v;
+        ul.appendChild(li);
+    })
+
+    document.body.appendChild(ul);
+}
